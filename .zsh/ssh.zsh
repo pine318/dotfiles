9aaad9c898177c0b08b0844838bfa527f4252b4c
Shun Matsumoto <s.matsumoto@uec.ac.jp>
2020-06-18 17:50:48 +0900

    ssh.zshの修正・makeコマンドをインストールするように変更

--- .zsh/ssh.zsh
+++ .zsh/ssh.zsh
@@ -10,18 +10,20 @@ function pfkill() {
 compdef _host pfkill
 
 # ssh-agent
-SSH_AGENT_FILE="$HOME/.ssh/ssh_agent"
+SSH_AGENT_FILE="${HOME}/.ssh/ssh_agent"
+AGENT_ADD_DIR="${HOME}/.ssh/agent_add"
 
-# 固定化された ssh-agent 設定が存在すれば、それを読み込んで
-# 既に起動している ssh-agent プロセスを再利用
-[ -f $SSH_AGENT_FILE ] && source $SSH_AGENT_FILE >& /dev/null
+if command -v ssh-agent 1>/dev/null 2>&1; then
+  # 固定化された ssh-agent 設定が存在すれば、それを読み込んで
+  # 既に起動している ssh-agent プロセスを再利用
+  [ -f $SSH_AGENT_FILE ] && source $SSH_AGENT_FILE >& /dev/null
 
-# ssh-add がエラーを返すなら、ssh-agent プロセスが存在しないものとして
-# これを起動しつつ、設定をファイルに出力
-if [ $( ps -ef | grep ssh-agent | grep -v grep | wc -l ) -eq 0 ]; then
-  ssh-agent >| $SSH_AGENT_FILE
-  # ssh-agent の設定内容を標準出力してほしいなら、/dev/null へ投げなくても良い
-  source $SSH_AGENT_FILE >& /dev/null
-  # $HOME/.ssh/agent_add 配下にある id_rsa という名前のファイルを ssh-add
-  [ -d $HOME/.ssh/agent_add ] && find $HOME/.ssh/agent_add -name id_rsa | xargs ssh-add >& /dev/null
+  # ssh-agent プロセスが存在しないならば、これを起動しつつ、設定をファイルに出力
+  if [ $( ps -ef | grep ssh-agent | grep -v grep | wc -l ) -eq 0 ]; then
+    ssh-agent >| $SSH_AGENT_FILE
+    # ssh-agent の設定内容を標準出力してほしいなら、/dev/null へ投げなくても良い
+    source $SSH_AGENT_FILE >& /dev/null
+    # $AGENT_ADD_DIR 配下にある id_rsa という名前のファイルを ssh-add
+    [ -d $AGENT_ADD_DIR ] && find $AGENT_ADD_DIR -name id_rsa | xargs ssh-add >& /dev/null
+  fi
 fi
--- etc/01-zsh_setup.sh
+++ etc/01-zsh_setup.sh
@@ -4,6 +4,6 @@ set -e
 
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install zsh peco
+sudo apt -y install zsh peco make
 chsh -s /usr/bin/zsh
 exit
