5bfe15a9c7af77d14565bce80a2bb304e482875d
pine18 <pine181818@gmail.com>
2020-12-16 19:31:24 +0900

    proxy関連修正，wsl-ssh-agent導入に伴う変更

---
+++ .zsh/proxy.zsh
@@ -0,0 +1,50 @@
+# proxy setting
+function set_proxy () {
+  PROXY=$1
+  SOCKS=$2
+  NO_PROXY=$3
+  export http_proxy=${PROXY}
+  export https_proxy=${PROXY}
+  export all_proxy=${SOCKS}
+  export no_proxy=${NO_PROXY}
+  export HTTP_PROXY=${PROXY}
+  export HTTPS_PROXY=${PROXY}
+  export ALL_PROXY=${SOCKS}
+  export NO_PROXY=${NO_PROXY}
+  echo "proxy=${PROXY}" >| ~/.curlrc
+  echo "http_proxy: ${PROXY}" >| ~/.gemrc
+  echo -e "use_proxy=on\nhttp_proxy=${PROXY}\nhttps_proxy=${PROXY}\nftp_proxy=${PROXY}" >| ~/.wgetrc
+  git config --global http.proxy ${PROXY}
+  git config --global https.proxy ${PROXY}
+}
+
+function unset_proxy() {
+  unset http_proxy
+  unset https_proxy
+  unset all_proxy
+  unset no_proxy
+  unset HTTP_PROXY
+  unset HTTPS_PROXY
+  unset ALL_PROXY
+  unset NO_PROXY
+  rm -f ~/.gemrc
+  rm -f ~/.wgetrc
+  rm -f ~/.curlrc
+  git config --get http.proxy &> /dev/null && git config --global --remove-section http
+  git config --get https.proxy &> /dev/null && git config --global --remove-section https
+}
+
+function set_uec_proxy () {
+  UEC_PROXY="http://proxy.uec.ac.jp:8080"
+  UEC_SOCKS="socks5://socks.cc.uec.ac.jp:1080"
+  YOSHIULAB_NO_PROXY="localhost,192.168.11.0/24"
+  set_proxy ${UEC_PROXY} ${UEC_SOCKS} ${YOSHIULAB_NO_PROXY}
+  git config --get http.http://192.168.11.11/gitbucket/.proxy &> /dev/null && git config --global --remove-section http.http://192.168.11.11/gitbucket/
+  git config --global http.http://192.168.11.11/gitbucket/.proxy ""
+}
+
+function unset_uec_proxy () {
+  unset_proxy
+  git config --get http.http://192.168.11.11/gitbucket/.proxy &> /dev/null && git config --global --remove-section http.http://192.168.11.11/gitbucket/
+  git config --global http.http://192.168.11.11/gitbucket/.proxy socks5://localhost:1080
+}
