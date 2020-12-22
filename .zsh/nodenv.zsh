1c4656f85cfafcc22a4ce55b160db6706136d5e5
pine18 <pine181818@gmail.com>
2020-12-22 23:26:04 +0900

    *envの細かい修正

--- .zsh/nodenv.zsh
+++ .zsh/nodenv.zsh
@@ -4,7 +4,6 @@ export PATH="${NODENV_ROOT}/bin:$PATH"
 
 if command -v nodenv 1>/dev/null 2>&1; then
   eval "$(nodenv init -)"
+  # remove PATH of nodenv from PATH of brew
+  alias brew="env PATH='${PATH/\/home\/${USER}\/\.nodenv\/shims:/}' brew"
 fi
-
-# remove PATH of nodenv from PATH of brew
-alias brew="env PATH='${PATH/\/home\/${USER}\/\.nodenv\/shims:/}' brew"
--- .zsh/pyenv.zsh
+++ .zsh/pyenv.zsh
@@ -3,24 +3,24 @@ export PYENV_ROOT="${HOME}/.pyenv"
 export PATH="${PYENV_ROOT}/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
-fi
-# remove PATH of pyenv from PATH of brew
-alias brew="env PATH='${PATH/\/home\/${USER}\/\.pyenv\/shims:/}' brew"
+  # remove PATH of pyenv from PATH of brew
+  alias brew="env PATH='${PATH/\/home\/${USER}\/\.pyenv\/shims:/}' brew"
 
-# pip
-if command -v pip 1>/dev/null 2>&1; then
-  # zsh completion
-  eval "$(pip completion --zsh)"
+  # pip
+  if command -v pip 1>/dev/null 2>&1; then
+    # zsh completion
+    eval "$(pip completion --zsh)"
 
-  # upgrade-all
-  alias pip-upgrade-all="pip list -o | tail -n +3 | awk '{ print \$1 }' | xargs pip install -U"
-fi
+    # upgrade-all
+    alias pip-upgrade-all="pip list -o | tail -n +3 | awk '{ print \$1 }' | xargs pip install -U"
 
-# pipenv
-if command -v pipenv 1>/dev/null 2>&1; then
-  # venv location
-  export PIPENV_VENV_IN_PROJECT=true
+    # pipenv
+    if command -v pipenv 1>/dev/null 2>&1; then
+      # venv location
+      export PIPENV_VENV_IN_PROJECT=true
 
-  # completion
-  eval "$(pipenv --completion)"
+      # completion
+      eval "$(pipenv --completion)"
+    fi
+  fi
 fi
