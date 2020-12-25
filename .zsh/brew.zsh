43a2f69942d0c8d9738b0b08a89d97435ede6df4
pine18 <pine181818@gmail.com>
2020-12-25 14:56:12 +0900

    linuxbrewのPATH変更

--- .zsh/brew.zsh
+++ .zsh/brew.zsh
@@ -6,3 +6,6 @@ elif [ -d /home/linuxbrew ]; then
   eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
   umask 002
 fi
+
+# PATH for linuxbrew
+alias brew="PATH=/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games: brew"
