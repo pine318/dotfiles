c865d5ce5a97b52b23882b209609c0cd2796e346
pine18 <pine181818@gmail.com>
2021-01-07 20:13:22 +0900

    subversion追加

--- .zsh/proxy.zsh
+++ .zsh/proxy.zsh
@@ -1,7 +1,9 @@
 # proxy setting
-function set_proxy () {
-  PROXY=$1
-  NO_PROXY=$2
+function set_proxy() {
+  PROXY_HOST=$1
+  PROXY_PORT=$2
+  PROXY="${PROXY_HOST}:${PROXY_PORT}"
+  NO_PROXY=$3
   export http_proxy=${PROXY}
   export https_proxy=${PROXY}
   export all_proxy=${PROXY}
@@ -10,9 +12,12 @@ function set_proxy () {
   export HTTPS_PROXY=${PROXY}
   export ALL_PROXY=${PROXY}
   export NO_PROXY=${NO_PROXY}
-  echo "proxy=${PROXY}" >| ~/.curlrc
-  echo "http_proxy: ${PROXY}" >| ~/.gemrc
-  echo -e "use_proxy=on\nhttp_proxy=${PROXY}\nhttps_proxy=${PROXY}\nftp_proxy=${PROXY}" >| ~/.wgetrc
+  echo "proxy=${PROXY}" >|~/.curlrc
+  echo "http_proxy: ${PROXY}" >|~/.gemrc
+  echo -e "use_proxy=on\nhttp_proxy=${PROXY}\nhttps_proxy=${PROXY}\nftp_proxy=${PROXY}" >|~/.wgetrc
+  if [ -e ~/.subversion/servers ] && ! grep "^http-proxy-host = " ~/.subversion/servers >/dev/null; then
+    echo -e "http-proxy-host = ${PROXY_HOST}\nhttp-proxy-port = ${PROXY_PORT}" >>~/.subversion/servers
+  fi
   git config --global http.proxy ${PROXY}
   git config --global https.proxy ${PROXY}
 }
@@ -26,23 +31,28 @@ function unset_proxy() {
   unset HTTPS_PROXY
   unset ALL_PROXY
   unset NO_PROXY
+  rm -f ~/.curlrc
   rm -f ~/.gemrc
   rm -f ~/.wgetrc
-  rm -f ~/.curlrc
-  git config --get http.proxy &> /dev/null && git config --global --remove-section http
-  git config --get https.proxy &> /dev/null && git config --global --remove-section https
+  if [ -e ~/.subversion/servers ]; then
+    sed -i -e '/^http-proxy-host = .*/d' ~/.subversion/servers
+    sed -i -e '/^http-proxy-port = \d*/d' ~/.subversion/servers
+  fi
+  git config --get http.proxy &>/dev/null && git config --global --remove-section http
+  git config --get https.proxy &>/dev/null && git config --global --remove-section https
 }
 
-function set_uec_proxy () {
-  UEC_PROXY="http://proxy.uec.ac.jp:8080"
+function set_uec_proxy() {
+  UEC_PROXY_HOST="http://proxy.uec.ac.jp"
+  UEC_PROXY_PORT="8080"
   YOSHIULAB_NO_PROXY="localhost,192.168.11.1,192.168.11.11"
-  set_proxy ${UEC_PROXY} ${YOSHIULAB_NO_PROXY}
-  git config --get http.http://192.168.11.11/gitbucket/.proxy &> /dev/null && git config --global --remove-section http.http://192.168.11.11/gitbucket/
+  set_proxy ${UEC_PROXY_HOST} ${UEC_PROXY_PORT} ${YOSHIULAB_NO_PROXY}
+  git config --get http.http://192.168.11.11/gitbucket/.proxy &>/dev/null && git config --global --remove-section http.http://192.168.11.11/gitbucket/
   git config --global http.http://192.168.11.11/gitbucket/.proxy ""
 }
 
-function unset_uec_proxy () {
+function unset_uec_proxy() {
   unset_proxy
-  git config --get http.http://192.168.11.11/gitbucket/.proxy &> /dev/null && git config --global --remove-section http.http://192.168.11.11/gitbucket/
+  git config --get http.http://192.168.11.11/gitbucket/.proxy &>/dev/null && git config --global --remove-section http.http://192.168.11.11/gitbucket/
   git config --global http.http://192.168.11.11/gitbucket/.proxy socks5://localhost:1080
 }
