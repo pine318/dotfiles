945491d87f1fc54cab01d0894c0ce98142a548ae
pine318 <pine181818@gmail.com>
2021-02-16 00:13:56 +0900

    brew bundleの前に.zshrcの読み込み処理を追加

--- etc/04-brew_setup.zsh
+++ etc/04-brew_setup.zsh
@@ -5,5 +5,6 @@ sudo localedef -f UTF-8 -i en_US en_US
 sudo localedef -f UTF-8 -i ja_JP ja_JP
 sudo -K
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+source ${HOME}/.zshrc
 brew bundle
 exit
